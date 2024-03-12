6cf883584ba114e782a1b4a325b0befccd25ded7
mlgrzt <goskolarska@gmail.com>
2024-03-12 22:18:52 +0100

    typo fix

--- manufura-nextjs/components/single/index.js
+++ manufura-nextjs/components/single/index.js
@@ -3,7 +3,7 @@ import SingleBenefits from "./02_benefits/SingleBenefits";
 import SingleVideo from "./03_video/SingleVideo";
 import SingleRelated from "./04_related/SingleRelated";
 import SingleGallery from "./05_gallery/SingleGallery";
-import SingleFaq from "./06_faq/SingleFaq";
+import SingleFaq from "./06_faq/SingleFAQ";
 
 export {
   SingleHero,
