fe8d2700ec1d6dbcbebd7302741191a2799980ee
mlgrzt <goskolarska@gmail.com>
2024-03-13 12:46:19 +0100

    changes in next config

--- manufura-nextjs/next.config.js
+++ manufura-nextjs/next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = {
+const nextConfig = {
   images: {
     domains: ["cdn.sanity.io"],
   },
+  output: "export",
 };
+
+module.exports = nextConfig;
